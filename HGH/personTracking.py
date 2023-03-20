07514f8965b1d3a71f158b357f7ae426a3854d35
NounL <88358909+NounL@users.noreply.github.com>
2023-03-20 17:26:39 -0400

    Update personTracking.py
    Included use of Facial Cascade Classifier for more reliable person detection when HOG descriptor not available

--- HGH/personTracking.py
+++ HGH/personTracking.py
@@ -1,3 +1,9 @@
+'''
+adapted from:
+https://realpython.com/face-recognition-with-python/
+https://thedatafrog.com/en/articles/human-detection-video/
+'''
+
 import numpy as np
 import cv2
  
@@ -5,29 +11,30 @@ import cv2
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-cv2.startWindowThread()
+# initialize facial cascade classifier
+cascPath = "./haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
 
-# open webcam video stream
+# open webcam and start cv window
+cv2.startWindowThread()
 cap = cv2.VideoCapture(0)
 
 while(True):
     ret, frame = cap.read()
 
-    # resizing for faster detection
-#     frame = cv2.resize(frame, (640, 480))
-    # using a greyscale picture, also for faster detection
+    # resizing and turn grayscale for faster detection
+    frame = cv2.resize(frame, (640, 480))
     gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
-    # detect people in the image
-    # returns the bounding boxes for the detected objects
+    # detect people in the image and return bouding box and weights
     detections = []
-    detections = hog.detectMultiScale(gray, winStride=(8,8) )
-    
-#     print(detections)
-    # create array "boxes" containing x , y position and width and height from x and y
-#     boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in detections[0]])
+    detections = hog.detectMultiScale(gray, winStride=(8,8))
 
+    # detect faces in the image and return bounding box
+    faces = faceCascade.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors = 5,
+        minSize = (30,30), flags = cv2.CASCADE_SCALE_IMAGE)
     
+    # loop over fulll body detections and draw boxes with 
     for i in range(len(detections[0])):
         (x, y, w, h) = detections[0][i]
         (xA, yA, xB, yB) = (x, y, x+w, y+h)
@@ -36,8 +43,15 @@ while(True):
         # display the detected boxes in the colour picture
         cv2.rectangle(frame, (xA, yA), (xB, yB),(0, 255, 255), 1)
         cv2.putText(frame, str(weight), (xA, yA),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
-                
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.2, (0,255,0), 2)
+    
+    # draw bounding box around estimated body (a body is about 7 heads tall and 1.5 heads wide)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame,
+                      (int(x-w/2),int(y-h/2)),
+                      (int(x+w*1.5), int(y+h*7)),
+                      (255,255,0), 1)
+    
     # Display the resulting frame
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
