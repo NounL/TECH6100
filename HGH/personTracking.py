e007f9fd2162030e55faa0b32698c74561ed7ac4
NounL <88358909+NounL@users.noreply.github.com>
2023-03-20 18:35:38 -0400

    Update personTracking.py

--- HGH/personTracking.py
+++ HGH/personTracking.py
@@ -25,16 +25,33 @@ while(True):
     # resizing and turn grayscale for faster detection
     frame = cv2.resize(frame, (640, 480))
     gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-
-    # detect people in the image and return bouding box and weights
-    detections = []
-    detections = hog.detectMultiScale(gray, winStride=(8,8))
-
+    
+    # reset mask to blank output
+    masked_img = np.zeros((480,640), np.uint8)
+    
     # detect faces in the image and return bounding box
     faces = faceCascade.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors = 5,
         minSize = (30,30), flags = cv2.CASCADE_SCALE_IMAGE)
     
-    # loop over fulll body detections and draw boxes with 
+    # draw bounding box around estimated body (a body is about 7 heads tall and within 2 heads wide)
+    for (x, y, w, h) in faces:
+        (xA, yA, xB, yB) = (int(x-w),
+                            int(y-h/2),
+                            int(x+w*2),
+                            int(y+h*7))
+        # draw rectanngle
+        cv2.rectangle(frame, (xA,yA), (xB, yB), (255,255,0), 1)
+        cv2.rectangle(masked_img, (xA,yA), (xB, yB), (255,255,255), -1)
+        
+        # mask image using only detected areas
+        masked_img = cv2.bitwise_and(gray, gray, mask = masked_img)
+        
+    
+    # detect people from masked image in and return bouding box and weights
+    detections = []
+    detections = hog.detectMultiScale(masked_img, winStride=(8,8))
+    
+    # loop over full body detections and draw boxes with 
     for i in range(len(detections[0])):
         (x, y, w, h) = detections[0][i]
         (xA, yA, xB, yB) = (x, y, x+w, y+h)
@@ -45,15 +62,10 @@ while(True):
         cv2.putText(frame, str(weight), (xA, yA),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.2, (0,255,0), 2)
     
-    # draw bounding box around estimated body (a body is about 7 heads tall and 1.5 heads wide)
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame,
-                      (int(x-w/2),int(y-h/2)),
-                      (int(x+w*1.5), int(y+h*7)),
-                      (255,255,0), 1)
     
     # Display the resulting frame
     cv2.imshow('frame',frame)
+    cv2.imshow("masked", masked_img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
